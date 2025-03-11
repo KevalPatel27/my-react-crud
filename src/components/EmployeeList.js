010c55cf55ac2893d87315e80a29bcb1358437d3
KevalPatel27 <cnc.kevalpatel27410@gmail.com>
2025-03-11 11:48:53 +0530

    Update EmployeeList.js

--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -22,12 +22,6 @@ const EmployeeList = () => {
             order: prev.key === key && prev.order === 'asc' ? 'desc' : 'asc',
         }))
     } )
-   
-    // const sortedEmployees = [...employees].sort((a, b) => {
-    //     if (a[sortConfig.key] < b[sortConfig.key]) return sortConfig.order === "asc" ? -1 : 1;
-    //     if (a[sortConfig.key] > b[sortConfig.key]) return sortConfig.order === "asc" ? 1 : -1;
-    //     return 0;
-    // });
 
     const sortedEmployees = [...employees].sort((a ,b) =>{
         if (a[sortConfig.key] < b[sortConfig.key]) return sortConfig.order === "asc" ? -1: 1;
